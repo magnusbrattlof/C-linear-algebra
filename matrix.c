e0376f0a40d6ede95ce9376aee4e79b3ab4c4365
Magnus Brattlöf <brattlof.magnus@gmail.com>
2018-01-17 18:58:55 +0100

    Modified determinant execution

--- matrix.c
+++ matrix.c
@@ -17,16 +17,43 @@ float det(matrix A) {
     }
     else if (A.i && A.j == 3) {
 
-        C = \
-        A.d[0][0] * (A.d[1][1] * A.d[2][2] - A.d[1][2] * A.d[2][1]) + \
-        -1 * A.d[0][1] * (A.d[1][0] * A.d[2][2] - A.d[1][2] * A.d[2][0]) + \
-        A.d[0][2] * (A.d[1][0] * A.d[2][1] - A.d[1][1] * A.d[2][0]);
+        float TMP[4];
 
-        if (C != 0) {
-            return C;
-        }
+        int p = 0;
+        int a = 0;
+        int x = 0;
+        int n = -1;
 
-        return 0;
+        for (int q = 0; q <= 2; q++) {
+            for (int row = 0; row <= 2; row++) {
+                for (int col = 0; col <= 2; col++) {
+
+                    // Check if row and column deviate from outer col and row
+                    if (row != p && col != q) {
+                        TMP[a++]= A.d[row][col];
+                        x++;
+
+                        if (x % 4 == 0) {
+                            n = -1;
+
+                            if (q % 2 == 0) {
+                                n = 1;
+                            }
+
+                            C += (n * A.d[p][q]) * ((TMP[0] * TMP[3]) - (TMP[1] * TMP[2]));
+
+                            // Clean up and reset
+                            x = 0;
+                            a = 0;
+                            for (int i = 0; i <= 3; i++) {
+                                TMP[i] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        return C;
     }
 
     else {
@@ -42,3 +69,8 @@ float* cofactor(matrix A) {
 
     return 0;
 }
+
+// C = \
+// A.d[0][0] * (A.d[1][1] * A.d[2][2] - A.d[1][2] * A.d[2][1]) + \
+// -1 * A.d[0][1] * (A.d[1][0] * A.d[2][2] - A.d[1][2] * A.d[2][0]) + \
+// A.d[0][2] * (A.d[1][0] * A.d[2][1] - A.d[1][1] * A.d[2][0]);
